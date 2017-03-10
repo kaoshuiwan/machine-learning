f39d588530a730719a4493f92707f7baf32b5c28
kaoshuiwan <zp10211059@163.com>
2017-03-10 18:26:18 +0800

    smo implemention of MLiA

---
+++ smoMLiA.py
@@ -0,0 +1,23 @@
+def loadDataSet(filename):
+    dataMat = []; labelMat = []
+    fr = open(filename)
+    for line in fr.readlines():
+        lineArr = line.strip().split('\t')
+        dataMat.append([float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(float(lineArr[2]))
+
+    return dataMat, labelMat
+
+def selectJrand(i, m):
+    j = i
+    while (j==i):
+        j = int(random.uniform(0, m))
+    return j
+
+def clipAlpha(aj, H, L):
+    if aj > H:
+        return H
+    else if aj < L:
+        return L
+    else:
+        return aj
